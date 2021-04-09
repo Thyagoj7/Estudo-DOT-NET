07df421a20221b3add0fbd8804701ef99c947585
Thyago <t.y_josef7@hotmail.com>
2021-04-09 08:59:48 -0300

    Uri

---
+++ AlgoritimoCDot/Uri-1002 AreaDoCirculo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Uri_1002_AreaDoCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double n = 3.14159;
+            double raio=0;
+            double area;
+            double y = 2;
+
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            raio = Math.Pow(raio, y);
+
+            area= raio * n;
+
+            area= Math.Round(area, 4);
+
+         
+            
+
+            Console.WriteLine("A =" + area);
+
+
+
+
+
+
+
+
+
+
+        }
+       
+    }
+}
